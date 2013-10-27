7754a0087fdb7d7485b4b14df1ac0d05e646092f
hp-renhuozhu <ren-huo.zhu@hp.com>
2013-10-27 23:22:18 +0800

    add the Display to the Model

--- InteriorDesign/InteriorDesign.Model/End/AdminUser.cs
+++ InteriorDesign/InteriorDesign.Model/End/AdminUser.cs
@@ -13,28 +13,33 @@ namespace InteriorDesign.Model.End
     public class AdminUser : BaseModel
     {
         [MaxLength(50)]
+        [Display(Name="管理员名称")]
         public string AdminName { get; set; }
         [MaxLength(50)]
+        [Display(Name = "管理员密码")]
         public string AdminPwd { get; set; }
-
+        [Display(Name = "管理员真实名称")]
         public string RealName { get; set; }
-
+        [Display(Name = "管理员最后登录时间")]
         public DateTime? LastLoginDate { get; set; }
-
+        [Display(Name = "管理员最后登录IP")]
         public string LastLoginIp { get; set; }
         /// <summary>
         /// 0超级管理员1普通管理员
         /// </summary>
+        [Display(Name = "管理员管理员类型")]
         public int AdminType { get; set; }
         /// <summary>
         /// 登录次数
         /// </summary>
+        [Display(Name = "管理员登录次数")]
         public int LoginAmount { get; set; }
         /// <summary>
         /// 手机号码
         /// </summary>
+        [Display(Name = "管理员手机号码")]
         public string Telephone { get; set; }
-
+        [Display(Name = "QQ")]
         public string QQ { get; set; }
 
     }
--- InteriorDesign/InteriorDesign.Model/Front/PagedPeoPle.cs
+++ InteriorDesign/InteriorDesign.Model/Front/PagedPeoPle.cs
@@ -6,13 +6,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace InteriorDesign.Model.Front
 {
+    /// <summary>
+    /// 员工表
+    /// </summary>
     public class PagedPeoPle : BaseModel
     {
         [Required(ErrorMessage = "人员的名称不能为空！")]
+        [Display(Name = "员工名称")]
         public string Name { get; set; }
         [Required(ErrorMessage = "人员的年龄不能为空！")]
+        [Display(Name = "员工年龄")]
         public int Age { get; set; }
-
+        [Display(Name = "员工性别")]
         public bool Sex { get; set; }
     }
 }
